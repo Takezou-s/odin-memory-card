e99f91b9dca21bc3df36fb393a62ce8a721afe0c
Takezou0 <bkaya1228@gmail.com>
2023-09-28 02:37:43 +0300

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Loading2, LoadingCard } from "./components/Card";
+import CardContainer from "./components/CardContainer";
+
+function App() {
+  return (
+    <>
+      <CardContainer count={15} />
+    </>
+  );
+}
+
+export default App;
